655047e4c19e1e20214be49f06c574315534bceb
Zachary Shannon <zachary.t.shan@gmail.com>
2018-08-07 10:41:04 -0400

    Changes

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -1,8 +1,17 @@
 package com.revature.caliber;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+
+import com.splunk.Args;
+import com.splunk.Receiver;
+import com.splunk.Service;
+
 
 @SpringBootApplication
 @ImportResource("classpath:spring-security.xml")
@@ -10,6 +19,27 @@ public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+		final Logger logger = Logger.getLogger(Application.class);
+		logger.info("Hello world");
+		
+		Map<String, Object> connectionArgs = new HashMap<String, Object>();
+		connectionArgs.put("host", "yourmom");
+		connectionArgs.put("username", "najibismail95");
+		connectionArgs.put("password", "Nj_6149746525");
+		connectionArgs.put("port", "8088");
+		connectionArgs.put("scheme", "https");
+		
+		Service splunkService = Service.connect(connectionArgs);
+		Receiver receiver = splunkService.getReceiver();
+		
+		Args logArgs = new Args();
+		logArgs.put("sourcetype", "hellosplunk");
+		
+		receiver.log("main", logArgs, "HelloSplunk Event");
+		
+		
+		
 	}
 	
 }
--- src/main/java/com/revature/caliber/aspect/LoggerAspect.java
+++ src/main/java/com/revature/caliber/aspect/LoggerAspect.java
@@ -6,64 +6,63 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-@Component("LogAspect")
-@Aspect
-public class LoggerAspect {
-	final static Logger logger = Logger.getLogger(LoggerAspect.class);
-	
-	/*
-	 * Mailer
-	 * sendEmails()
-	 */
-	@AfterReturning("execution(* com.revature.caliber.email.Mailer.sendEmails(..))")
-	public void successEmailSend() {
-		
-	}
-	
-	@AfterThrowing("execution(* com.revature.caliber.email.Mailer.sendEmails(..))")
-	public void failEmailSend() {
-		
-	}
-	
-	/*
-	 * Mailer
-	 * getEmailString()
-	 */
-	@AfterReturning("execution(* com.revature.caliber.email.Mailer.getEmailString(..))")
-	public void successGetEmailString() {
-		
-	}
-	
-	@AfterThrowing("execution(* com.revature.caliber.email.Mailer.getEmailString(..))")
-	public void failGetEmailString() {
-		
-	}
-	
-	/*
-	 * FlagAlertMailer
-	 * sendEmails()
-	 */
-	@AfterReturning("execution(* com.revature.caliber.email.FlagAlertEmailer.sendEmails(..))")
-	public void successFlagEmailSend() {
-		
-	}
-	
-	@AfterThrowing("execution(* com.revature.caliber.email.FlagAlertEmailer.sendEmails(..))")
-	public void failFlagEmailSend() {
-		
-	}
-	
-	/*
-	 * FlagAlerMailer
-	 * getFlagEmailString()
-	 */
-	@AfterReturning("execution(* com.revature.caliber.email.FlagAlertEmailer.getFlagEmailString(..))")
-	public void successGetFlagEmailString() {
-		
-	}
-	
-	@AfterThrowing("execution(* com.revature.caliber.email.FlagAlertEmailer.getFlagEmailString(..))")
-	public void failGetFlagEmailString() {
-		
-	}	
-}
+//@Component("LogAspect")
+//@Aspect
+//public class LoggerAspect {
+//	final static Logger logger = Logger.getLogger(LoggerAspect.class);
+//	
+//	/*
+//	 * Mailer
+//	 * sendEmails()
+//	 */
+//	@AfterReturning("execution(* com.revature.caliber.email.Mailer.sendEmails(..))")
+//	public void successEmailSend() {
+//		
+//	}
+//	
+//	@AfterThrowing("execution(* com.revature.caliber.email.Mailer.sendEmails(..))")
+//	public void failEmailSend() {
+//		
+//	}
+//	
+//	/*
+//	 * Mailer
+//	 * getEmailString()
+//	 */
+//	@AfterReturning("execution(* com.revature.caliber.email.Mailer.getEmailString(..))")
+//	public void successGetEmailString() {
+//		
+//	}
+//	
+//	@AfterThrowing("execution(* com.revature.caliber.email.Mailer.getEmailString(..))")
+//	public void failGetEmailString() {
+//		
+//	}
+//	
+//	/*
+//	 * FlagAlertMailer
+//	 * sendEmails()
+//	 */
+//	@AfterReturning("execution(* com.revature.caliber.email.FlagAlertEmailer.sendEmails(..))")
+//	public void successFlagEmailSend() {
+//		
+//	}
+//	
+//	@AfterThrowing("execution(* com.revature.caliber.email.FlagAlertEmailer.sendEmails(..))")
+//	public void failFlagEmailSend() {
+//		
+//	}
+//	
+//	/*
+//	 * FlagAlerMailer
+//	 * getFlagEmailString()
+//	 */
+//	@AfterReturning("execution(* com.revature.caliber.email.FlagAlertEmailer.getFlagEmailString(..))")
+//	public void successGetFlagEmailString() {
+//		
+//	}
+//	
+//	@AfterThrowing("execution(* com.revature.caliber.email.FlagAlertEmailer.getFlagEmailString(..))")
+//	public void failGetFlagEmailString() {
+//		
+//	}	
