cab87e686088f597e4062c20963491bf5400e892
davidyang11 <yangd5987@gmail.com>
2018-08-07 15:11:50 -0400

    David -- Commit Email 2

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -3,11 +3,13 @@ package com.revature.caliber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ImportResource("classpath:spring-security.xml")
+@EnableEurekaClient
 public class Application {
 	
 	public static void main(String[] args) {
