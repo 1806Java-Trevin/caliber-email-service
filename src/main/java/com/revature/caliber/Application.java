d2c9355fa0d8f0dc4b0b30644ded874dc2715c4c
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-08-07 14:50:04 -0400

    Added Eureka Client

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -3,10 +3,12 @@ package com.revature.caliber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableEurekaClient
 @ImportResource("classpath:spring-security.xml")
 public class Application {
 	
