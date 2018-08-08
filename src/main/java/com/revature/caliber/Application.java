e76167084a39b0c8fcc5ee9fc8472e9331f4b755
Ezzdean Bietar <EzzdeanB@gmail.com>
2018-08-08 09:10:52 -0400

    Removed redundant annotations in main

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ImportResource;
 @EnableAutoConfiguration
 @EnableEurekaClient
 @ImportResource("classpath:spring-security.xml")
-@EnableEurekaClient
 public class Application {
 	
 	public static void main(String[] args) {
